4237412211bdbef05b1ccbf9653c6128a238a0c9
Alexaltrex <alexaltrex@mail.ru>
2020-08-25 16:46:25 +0400

    Updates

---
+++ precache-manifest.7bc4fb6ea6e0180eb1205dcd2740f8ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73970273952555998bac1967a1221b44",
+    "url": "/Rick-and-Morty-DB/index.html"
+  },
+  {
+    "revision": "3c9afbc7e42c106e75cc",
+    "url": "/Rick-and-Morty-DB/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fda63f0c875e5dd9c69d",
+    "url": "/Rick-and-Morty-DB/static/js/2.87c0353c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Rick-and-Morty-DB/static/js/2.87c0353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9afbc7e42c106e75cc",
+    "url": "/Rick-and-Morty-DB/static/js/main.7dd60a01.chunk.js"
+  },
+  {
+    "revision": "10f7752b0e7b74e592b9",
+    "url": "/Rick-and-Morty-DB/static/js/runtime-main.330273c8.js"
+  }
+]);
