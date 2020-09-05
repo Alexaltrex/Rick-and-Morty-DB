d4166da771b05dfb236b63efbe94bca4f2b094d0
Alexaltrex <alexaltrex@mail.ru>
2020-09-05 16:24:44 +0400

    Updates

--- precache-manifest.e21490ecd47bf0b26647ca20dc5abc64.js
+++ precache-manifest.e21490ecd47bf0b26647ca20dc5abc64.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff77bde77e332716c659f301d3ed8181",
+    "revision": "c83e5f8a26813d2003fa79b471bd7da6",
     "url": "/Rick-and-Morty-DB/index.html"
   },
   {
-    "revision": "00a5171be5e45a4eae81",
+    "revision": "6c2e5240923d6d3ca0cb",
     "url": "/Rick-and-Morty-DB/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "ef7f46a38a454d19f541",
-    "url": "/Rick-and-Morty-DB/static/js/2.57a0ce08.chunk.js"
+    "revision": "4f1a3bfa0f1d9c03b79e",
+    "url": "/Rick-and-Morty-DB/static/js/2.a0068372.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/Rick-and-Morty-DB/static/js/2.57a0ce08.chunk.js.LICENSE.txt"
+    "url": "/Rick-and-Morty-DB/static/js/2.a0068372.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "00a5171be5e45a4eae81",
-    "url": "/Rick-and-Morty-DB/static/js/main.38c474fc.chunk.js"
+    "revision": "6c2e5240923d6d3ca0cb",
+    "url": "/Rick-and-Morty-DB/static/js/main.98d8e4a7.chunk.js"
   },
   {
     "revision": "10f7752b0e7b74e592b9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rick-and-Morty-DB/precache-manifest.0a494cb0bcd8ebec1336a2a4516920e9.js"
+  "/Rick-and-Morty-DB/precache-manifest.e21490ecd47bf0b26647ca20dc5abc64.js"
 );
 
 self.addEventListener('message', (event) => {
